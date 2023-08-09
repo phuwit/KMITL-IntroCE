685c2ecc4a9e41571d387a8fcbc52522015cfaa8
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2023-08-09 14:32:40 +0700

    toggling completed

--- Class4/4-1-InputSwitch.ino
+++ Class4/4-1-InputSwitch.ino
@@ -1,49 +1,39 @@
 const int BUTTON_COUNT = 3;
-const int BUTTON[BUTTON_COUNT] = {3, 4, 2};
-const char BUTTON_TYPE[BUTTON_COUNT] = {'D', 'U', 'I'};
+const int BUTTON[BUTTON_COUNT] = { 3, 4, 2 };
+const char BUTTON_TYPE[BUTTON_COUNT] = { 'D', 'U', 'I' };
 int buttonState[BUTTON_COUNT];
-int lastButtonState[BUTTON_COUNT] = {LOW};
-unsigned long lastDebounceTime[BUTTON_COUNT] = {0};  // the last time the output pin was toggled
-const unsigned long debounceDelay = 50;  // the debounce time; increase if the output flickers
+int lastButtonState[BUTTON_COUNT] = { LOW };
+unsigned long lastDebounceTime[BUTTON_COUNT] = { 0 };  // the last time the output pin was toggled
+const unsigned long debounceDelay = 50;                // the debounce time; increase if the output flickers
 
 const int LED_COUNT = 3;
-const int LED[LED_COUNT] = {12, 11, 10};
-int ledStatus[LED_COUNT] = {LOW, HIGH, HIGH};
+const int LED[LED_COUNT] = { 12, 11, 10 };
+int ledStatus[LED_COUNT] = { LOW };
 
 void setup() {
-    for (int i = 0; i < BUTTON_COUNT; i++) {
-        if (BUTTON_TYPE[i] == 'I') {
-            pinMode(BUTTON[i], INPUT_PULLUP);
-        }
-        else {
-            pinMode(BUTTON[i], INPUT);
-        }
+  for (int i = 0; i < BUTTON_COUNT; i++) {
+    if (BUTTON_TYPE[i] == 'I') {
+      pinMode(BUTTON[i], INPUT_PULLUP);
+    } else {
+      pinMode(BUTTON[i], INPUT);
     }
+  }
 
-    for (int i = 0; i < LED_COUNT; i++) {
-        pinMode(LED[i], OUTPUT);
-    }
+  for (int i = 0; i < LED_COUNT; i++) {
+    pinMode(LED[i], OUTPUT);
+  }
+
+  Serial.begin(9600);
 }
 
 void loop() {
-    for (int i = 0; i < BUTTON_COUNT; i++) {
-        deBounce(i);
-
-        // Active
-        ledStatus[i] = buttonState[i];
-
-        // // Toggle
-        // if (buttonState[i] == HIGH) {
-        //     ledStatus[i] = !ledStatus[i];
-        // }
-
-        digitalWrite(LED[i], ledStatus[i]);
-    }
-}
-
-void deBounce(int buttonIndex) {
+  for (int buttonIndex = 0; buttonIndex < BUTTON_COUNT; buttonIndex++) {
     // read the state of the switch into a local variable:
     int reading = digitalRead(BUTTON[buttonIndex]);
+
+    if (BUTTON_TYPE[buttonIndex] != 'D') {
+      reading = !reading;
+    }
 
     // check to see if you just pressed the button
     // (i.e. the input went from LOW to HIGH), and you've waited long enough
@@ -51,25 +41,31 @@ void deBounce(int buttonIndex) {
 
     // If the switch changed, due to noise or pressing:
     if (reading != lastButtonState[buttonIndex]) {
-        // reset the debouncing timer
-        lastDebounceTime[buttonIndex] = millis();
+      // reset the debouncing timer
+      lastDebounceTime[buttonIndex] = millis();
     }
 
     if ((millis() - lastDebounceTime[buttonIndex]) > debounceDelay) {
-        // whatever the reading is at, it's been there for longer than the
-        // debounce delay, so take it as the actual current state:
+      // whatever the reading is at, it's been there for longer than the debounce
+      // delay, so take it as the actual current state:
 
-        // if the button state has changed:
-        if (reading != buttonState[buttonIndex]) {
-            buttonState[buttonIndex] = reading;
+      // if the button state has changed:
+      if (reading != buttonState[buttonIndex]) {
+        buttonState[buttonIndex] = reading;
+
+        // only toggle the LED if the new button state is HIGH
+        if (buttonState[buttonIndex] == HIGH) {
+          ledStatus[buttonIndex] = !ledStatus[buttonIndex];
         }
+      }
     }
 
     // save the reading. Next time through the loop, it'll be the
     // lastButtonState:
     lastButtonState[buttonIndex] = reading;
 
-    if (BUTTON_TYPE[buttonIndex] != 'D') {
-        buttonState[buttonIndex] = !buttonState[buttonIndex];
-    }
+    digitalWrite(LED[buttonIndex], ledStatus[buttonIndex]);
+  }
+  Serial.print(buttonState[1]);
+  Serial.println(ledStatus[1]);
 }
