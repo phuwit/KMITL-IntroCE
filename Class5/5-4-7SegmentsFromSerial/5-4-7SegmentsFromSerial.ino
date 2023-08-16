9affca090887f868960c6e05ec32b55f07056de9
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2023-08-16 14:22:42 +0700

    5.4 done
    Co-authored-by: Dangkeys <136824908+Dangkeys@users.noreply.github.com>

--- Class5/5-4-7SegmentsFromSerial/5-4-7SegmentsFromSerial.ino
+++ Class5/5-4-7SegmentsFromSerial/5-4-7SegmentsFromSerial.ino
@@ -4,16 +4,17 @@ const uint8_t SEGMENT_PINS[NUM_PINS] = {2, 3, 4, 5, 6, 7, 8, 9};
 // input number to light up from array index then get a,b,c,... segments in index number from SEGMENT_PIN to map into pin number
 const uint8_t SEGMENT_TABLE[10][7] = {
 //   a  b  c  d  e  f  g
-    {1, 1, 1, 1, 1, 1, 0}, // 0
-    {0, 1, 1, 0, 0, 0, 0}, // 1
-    {1, 1, 0, 1, 1, 0, 1}, // 2
-    {1, 1, 1, 1, 0, 0, 0}, // 3
-    {0, 1, 1, 0, 0, 1, 1}, // 4
-    {1, 0, 1, 1, 0, 1, 1}, // 5
-    {1, 0, 1, 1, 1, 1, 1}, // 6
-    {1, 1, 1, 0, 0, 0, 0}, // 7
-    {1, 1, 1, 1, 1, 1, 1}, // 8
-    {1, 1, 1, 1, 0, 1, 1}, // 9
+    {0, 0, 0, 0, 0, 0, 1}, // 0
+    {1, 0, 0, 1, 1, 1, 1}, // 1
+    {0, 0, 1, 0, 0, 1, 0}, // 2
+    {0, 0, 0, 0, 1, 1, 0}, // 3
+    {1, 0, 0, 1, 1, 0, 0}, // 4
+    {0, 1, 0, 0, 1, 0, 0}, // 5
+    {0, 1, 0, 0, 0, 0, 0}, // 6
+    {0, 0, 0, 1, 1, 1, 1}, // 7
+    {0 ,0 ,0 ,0 ,0 ,0 ,0}, // 8
+    {0 ,0 ,0 ,0 ,1 ,0 ,0}  // 9
+
 };
 
 void setup() {
@@ -31,14 +32,18 @@ void loop() {
         Serial.println(incomingByte);
         
         if (incomingByteAsInt >= 0 && incomingByteAsInt <= 9) {
+            Serial.println("is int");
+
             for (int i = 0; i < 7; i++) {
+                // Serial.println("new loop");
+                // Serial.println(i);
+                // Serial.println(SEGMENT_PINS[i]);
+                // Serial.println(SEGMENT_TABLE[incomingByteAsInt][i]);
                 digitalWrite(SEGMENT_PINS[i], SEGMENT_TABLE[incomingByteAsInt][i]);
             }
         }
-        else if (incomingByte != 10)  {
+        else  {
             Serial.println("Input is not number");
         }
-
-        delay(500);
     }
 }
