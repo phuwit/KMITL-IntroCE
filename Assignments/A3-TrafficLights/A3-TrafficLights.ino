33c62013e22d24ed96c7047b8fed97aa1244818e
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2023-09-20 14:32:34 +0700

    rename variables and move to enums

--- Assignments/A3-TrafficLights/A3-TrafficLights.ino
+++ Assignments/A3-TrafficLights/A3-TrafficLights.ino
@@ -6,26 +6,23 @@
 #define LED_E_R 6
 #define LED_E_Y 5
 #define LED_E_G 4
-#define goN 0
-#define waitN 1
-#define goE 2
-#define waitE 3
 
 enum States { GoNorth, WaitNorth, GoEast, WaitEast };
 
 struct State {
-    unsigned long ST_Out;
+    unsigned long lightsData;
     // 6-bit pattern to street output
-    unsigned long Time;
+    unsigned long delay;
     // delay in ms units
-    unsigned long NextST[4];
+    unsigned long nextState[4];
 };  // next state for inputs 0,1,2,3
 typedef const struct State SType;
-SType FSM[4] = {{B00001100, 30000, {goN, waitN, goN, waitN}},
-                {B00010100, 5000, {goE, goE, goE, goE}},
-                {B00100001, 30000, {goE, goE, waitE, waitE}},
-                {B00100010, 5000, {goN, goN, goN, goN}}};
-unsigned long ST = 0;  // index to the current state
+SType stateData[4] = { {B00001100, 30000, {States::GoNorth, States::WaitNorth, States::GoNorth, States::WaitNorth}},
+                       {B00010100, 5000, {States::GoEast, States::GoEast, States::GoEast, States::GoEast}},
+                       {B00100001, 30000, {States::GoEast, States::GoEast, States::WaitEast, States::WaitEast}},
+                       {B00100010, 5000, {States::GoNorth, States::GoNorth, States::GoNorth, States::GoNorth}}
+                      };
+unsigned long currentState = 0;  // index to the current state
 
 void setup() {
     pinMode(LED_N_R, OUTPUT);
@@ -41,15 +38,15 @@ void setup() {
 int input, input1, input2;
 
 void loop() {
-    digitalWrite(LED_N_R, FSM[ST].ST_Out & B00100000);
-    digitalWrite(LED_N_Y, FSM[ST].ST_Out & B00010000);
-    digitalWrite(LED_N_G, FSM[ST].ST_Out & B00001000);
-    digitalWrite(LED_E_R, FSM[ST].ST_Out & B00000100);
-    digitalWrite(LED_E_Y, FSM[ST].ST_Out & B00000010);
-    digitalWrite(LED_E_G, FSM[ST].ST_Out & B00000001);
-    delay(FSM[ST].Time);
+    digitalWrite(LED_N_R, stateData[currentState].lightsData & B00100000);
+    digitalWrite(LED_N_Y, stateData[currentState].lightsData & B00010000);
+    digitalWrite(LED_N_G, stateData[currentState].lightsData & B00001000);
+    digitalWrite(LED_E_R, stateData[currentState].lightsData & B00000100);
+    digitalWrite(LED_E_Y, stateData[currentState].lightsData & B00000010);
+    digitalWrite(LED_E_G, stateData[currentState].lightsData & B00000001);
+    delay(stateData[currentState].delay);
     input1 = digitalRead(BUTTON_NORTH);
     input2 = digitalRead(BUTTON_EAST);
     input = input1 * 2 + input2;
-    ST = FSM[ST].NextST[input];
+    currentState = stateData[currentState].nextState[input];
 }
