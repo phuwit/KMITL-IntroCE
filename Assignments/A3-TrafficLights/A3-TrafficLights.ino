9b2a7304159ee888e100ff58aacf293ce0bacaf7
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2023-10-04 12:34:59 +0700

    feat: Traffic lights working without blinking

--- Assignments/A3-TrafficLights/A3-TrafficLights.ino
+++ Assignments/A3-TrafficLights/A3-TrafficLights.ino
@@ -1,11 +1,14 @@
-#define BUTTON_NORTH 3
-#define LED_N_R 10
-#define LED_N_Y 9
-#define LED_N_G 8
 #define BUTTON_EAST 2
-#define LED_E_R 6
-#define LED_E_Y 5
-#define LED_E_G 4
+#define BUTTON_NORTH 4
+#define BUTTON_PEDESTRIAN 3
+#define LED_NORTH_RED 13
+#define LED_NORTH_YELLOW 12
+#define LED_NORTH_GREEN 11
+#define LED_PEDESTRIAN_RED 9
+#define LED_PEDESTRIAN_GREEN 8
+#define LED_EAST_RED 7
+#define LED_EAST_YELLOW 6
+#define LED_EAST_GREEN 5
 
 enum States { NorthGreen, NorthYellow, EastGreen, EastYellow, Pedestrian, PedestrianBlink };
 
@@ -17,40 +20,54 @@ struct State {
     // delay in ms units
     States nextState[8];
 };  // next state for inputs 0,1,2,3
-typedef const struct State SType;
-SType stateData[sizeof(States)] = {
-    {b10001100, 2000, {NorthGreen, NorthYellow, NorthGreen, NorthYellow, NorthYellow, NorthYellow, NorthYellow, NorthYellow}},
-    {b10010100, 500,  {EastGreen, EastGreen, EastGreen, EastGreen, Pedestrian, Pedestrian, Pedestrian, Pedestrian}},
-    {b10100001, 2000, {EastGreen, EastGreen, EastYellow, EastYellow, EastYellow, EastYellow, EastYellow, EastYellow}},
-    {b10100010, 500,  {NorthGreen, NorthGreen, NorthGreen, NorthGreen, Pedestrian, Pedestrian, NorthGreen, NorthGreen}},
-    {b01100100, 2000, {Pedestrian, PedestrianBlink, PedestrianBlink, Pedestrian, PedestrianBlink, PedestrianBlink, PedestrianBlink}},
-    {b10100100, 500,  {EastGreen, EastGreen, NorthGreen, EastGreen, NorthGreen, NorthGreen, EastGreen, EastGreen}}
+const struct State stateData[6] = {
+    {B10001100, 2000, {NorthYellow, NorthYellow, NorthGreen, NorthYellow, NorthYellow, NorthYellow, NorthYellow, NorthYellow}},
+    {B10010100, 500,  {Pedestrian, EastGreen, EastGreen, EastGreen, Pedestrian, Pedestrian, Pedestrian, Pedestrian}},
+    {B10100001, 2000, {EastYellow, EastGreen, EastYellow, EastYellow, EastYellow, EastYellow, EastYellow, EastYellow}},
+    {B10100010, 500,  {NorthGreen, NorthGreen, NorthGreen, NorthGreen, Pedestrian, Pedestrian, NorthGreen, NorthGreen}},
+    {B01100100, 2000, {PedestrianBlink, PedestrianBlink, PedestrianBlink, PedestrianBlink, Pedestrian, PedestrianBlink, PedestrianBlink}},
+    {B10100100, 500,  {EastGreen, EastGreen, NorthGreen, EastGreen, NorthGreen, NorthGreen, EastGreen, EastGreen}}
 };
 unsigned long currentState = 0;  // index to the current state
 
 void setup() {
-    pinMode(LED_N_R, OUTPUT);
-    pinMode(LED_N_Y, OUTPUT);
-    pinMode(LED_N_G, OUTPUT);
-    pinMode(BUTTON_NORTH, INPUT);
-    pinMode(LED_E_R, OUTPUT);
-    pinMode(LED_E_Y, OUTPUT);
-    pinMode(LED_E_G, OUTPUT);
-    pinMode(BUTTON_EAST, INPUT);
+    Serial.begin(9600);
+
+    pinMode(BUTTON_NORTH, INPUT_PULLUP);
+    pinMode(BUTTON_EAST, INPUT_PULLUP);
+    pinMode(BUTTON_PEDESTRIAN, INPUT_PULLUP);
+    pinMode(LED_NORTH_RED, OUTPUT);
+    pinMode(LED_NORTH_YELLOW, OUTPUT);
+    pinMode(LED_NORTH_GREEN, OUTPUT);
+    pinMode(LED_PEDESTRIAN_RED, OUTPUT);
+    pinMode(LED_PEDESTRIAN_GREEN, OUTPUT);
+    pinMode(LED_EAST_RED, OUTPUT);
+    pinMode(LED_EAST_YELLOW, OUTPUT);
+    pinMode(LED_EAST_GREEN, OUTPUT);
 }
 
-int input, input1, input2;
+int input, inputNorth, inputEast, inputPedestrian;
 
 void loop() {
-    digitalWrite(LED_N_R, stateData[currentState].lightsData & B00100000);
-    digitalWrite(LED_N_Y, stateData[currentState].lightsData & B00010000);
-    digitalWrite(LED_N_G, stateData[currentState].lightsData & B00001000);
-    digitalWrite(LED_E_R, stateData[currentState].lightsData & B00000100);
-    digitalWrite(LED_E_Y, stateData[currentState].lightsData & B00000010);
-    digitalWrite(LED_E_G, stateData[currentState].lightsData & B00000001);
+    digitalWrite(LED_PEDESTRIAN_RED,   stateData[currentState].lightsData & B10000000);
+    digitalWrite(LED_PEDESTRIAN_GREEN, stateData[currentState].lightsData & B01000000);
+    digitalWrite(LED_NORTH_RED,        stateData[currentState].lightsData & B00100000);
+    digitalWrite(LED_NORTH_YELLOW,     stateData[currentState].lightsData & B00010000);
+    digitalWrite(LED_NORTH_GREEN,      stateData[currentState].lightsData & B00001000);
+    digitalWrite(LED_EAST_RED,         stateData[currentState].lightsData & B00000100);
+    digitalWrite(LED_EAST_YELLOW,      stateData[currentState].lightsData & B00000010);
+    digitalWrite(LED_EAST_GREEN,       stateData[currentState].lightsData & B00000001);
+
     delay(stateData[currentState].delay);
-    input1 = digitalRead(BUTTON_NORTH);
-    input2 = digitalRead(BUTTON_EAST);
-    input = input1 * 2 + input2;
+
+    inputNorth = !digitalRead(BUTTON_NORTH);
+    inputEast = !digitalRead(BUTTON_EAST);
+    inputPedestrian = !digitalRead(BUTTON_PEDESTRIAN);
+    input = inputEast + (inputNorth * 2) + (inputPedestrian * 4);
+    Serial.print("input : ");
+    Serial.println(input);
+
     currentState = stateData[currentState].nextState[input];
+    Serial.print("currentState : ");
+    Serial.println(currentState);
 }
